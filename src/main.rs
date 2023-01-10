e21bc49be1b39dbdd52190a42b19dd573af36015
reta-ygs <github@no-reply.reta.dev>
2023-01-10 22:58:59 +0900

    Respond to multiple PINGs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::io::Write;
-use std::net::TcpListener;
+use std::io::{BufRead, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -11,11 +10,19 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                _stream.write("+PONG\r\n".as_bytes()).unwrap();
+                handle_connection(_stream).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    let reader = std::io::BufReader::new(stream.try_clone()?);
+    for _line in reader.lines() {
+        stream.write("+PONG\r\n".as_bytes())?;
+    }
+    Ok(())
 }
